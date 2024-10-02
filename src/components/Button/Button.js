b4514471092943009692ca9ef7e24e409b733ac0
van-gomes <nninha.gomes@gmail.com>
2024-10-02 12:37:06 -0300

    Apply Open/Closed Principle

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-export function Button({ label, onClick, type, className, ...props }) {
+export function Button({ label, onClick, type, variant, icon, className, ...props }) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${styles.button} ${className}`}
+      className={`${styles.button} ${styles[variant]} ${className}`}
       {...props}
     >
+      {icon && <span className={styles.icon}>{icon}</span>}
       {label}
     </button>
   );
@@ -19,10 +20,13 @@ Button.propTypes = {
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  variant: PropTypes.oneOf(['primary', 'danger', 'success', 'warning']),
+  icon: PropTypes.node,
   className: PropTypes.string,
 };
 
 Button.defaultProps = {
   type: 'button',
+  variant: 'primary',
   className: '',
 };
