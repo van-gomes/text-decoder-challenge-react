2ef3d3666a618d54aa826a955eb8cc0fd4d1cf47
van-gomes <nninha.gomes@gmail.com>
2024-11-16 19:23:16 -0300

    Made components responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header>
-      <nav className={styles.container_header}>
+      <nav className={styles.container__header}>
         <a herf="#">
           <img src={img_logo} alt="Logo da Alura" />
         </a>
